f02133ae5a5068724bbfef3b9f33a816acdd921c
AnaZderic <zderic.ana20@gmail.com>
2023-01-14 18:39:44 +0100

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,33 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "🙏",
+      name: "Person With Folded Hands",
+      meaning:
+        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+        id: 4,
+        emoji: "🍔",
+        name: "Hamburger",
+        meaning:
+        "A burger with a beef patty, usually depicted on a sesame bun with cheese, lettuce, and tomato—making it a cheeseburger, though officially called Hamburger by Unicode."
+    },
+    
+  ];
+
+  export default emojipedia; 
